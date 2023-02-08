50a28ac412fccfc63f39a7a794da2f12f694f6d7
Salahuddin Mahmud <76897399+s-mahmud52@users.noreply.github.com>
2023-02-09 01:07:57 +0600

    userlist setup

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,99 +1,49 @@
-import React from 'react'
-import Image from './Image'
+import React, { useEffect, useState } from "react";
+import Image from "./Image";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from "react-redux";
 const UserList = () => {
-  return (
-    <div className='groupholder'>
-        <div className='titleholder'>
-            <h3>User List</h3>
-        </div>
-        <div className='boxholder'>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-            <div className='box'>
-                <div className='boxImgholder'>
-                    <Image imgSrc="assets/profile.png"/>
-                </div>
-                <div className='title'>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='boxbtn'>Add</button>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const db = getDatabase();
+  const data = useSelector((state) => state);
+  let [userlist, setUserlist] = useState([]);
 
-export default UserList
+  useEffect(() => {
+    const userRef = ref(db, "users");
+    onValue(userRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach(item => {
+          if(data.userdata.userInfo.uid != item.key){
+            arr.push(item.val());
+        }
+      });
+      setUserlist(arr);
+    });
+  }, []);
+  return (
+    <div className="groupholder">
+      <div className="titleholder">
+        <h3>User List</h3>
+      </div>
+      <div className="boxholder">
+        {userlist.map(item=>(
+            
+            <div className='box'>
+                <div className='boxImgholder'>
+                    <Image imgSrc="assets/profile.png"/>
+                </div>
+                <div className='title'>
+                    <h3>{item.displayName}</h3>
+                    <p>{item.email}</p>
+                </div>
+                <div>
+                    <button className='boxbtn'>Add</button>
+                </div>
+            </div>
+
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
